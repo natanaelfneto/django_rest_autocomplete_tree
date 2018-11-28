c4bd7dc797ea010f80e9de3f4418431ccc71637b
natanaelfneto <natanaelfneto@outlook.com>
2018-11-28 03:32:08 -0200

    updated commentaries and info about classes and functions in views

--- src/apps/api/views.py
+++ src/apps/api/views.py
@@ -16,6 +16,9 @@ class Patients:
     # initalize Patient instance
     def __init__(self, csv_file):
         """
+        Initialize instance of class
+        Argument(s): 
+            csv_file: A string with path to a file
         """
 
         # create a sorted string trie object instance
@@ -33,23 +36,29 @@ class Patients:
                 # add row with patient name to trie object
                 trie[''.join(row)] = index
 
-        # 
+        # add trie value to self
         self.trie = trie
     
     # create a suggestion object filter for patients, parsing the query argument
     def suggestions(self, query):
         """
+        Create and array of sugestions based on query
+        Argument(s):
+            query: A string with one or more letters to be parsed as substring of names
         """
 
-        # 
+        # return array of suggestions for patients names
         return { 'patients': self.trie.keys(prefix=query) }
         
 
-# 
+# rest framework api view
 @api_view(['GET'])
 def patient_suggestios(request, query):
     """
-    
+    Parses queries to be checked as substrings of patients names within tree structure
+    Argument(s):
+        request: A string representing the scheme of the request (http or https usually)
+        query: A string with one or more letters to be parsed as substring of names
     """
 
     # check for GET method request
